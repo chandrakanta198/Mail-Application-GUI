8d1714e4218eb87854ef669437857dc36369ac83
Chandrakanta <chandrakantasaini198@gmail.com>
2023-07-14 19:41:50 +0530

    main.py

--- main.py
+++ main.py
@@ -1,30 +1,25 @@
 import tkinter as tk
 from tkinter import messagebox, scrolledtext
 
-# Function to handle the send button click event
+# handle click event
 def send_mail():
     recipient = recipient_entry.get()
     subject = subject_entry.get()
     message = message_text.get("1.0", tk.END)
 
-    # Send the mail logic goes here
-    # ...
-
     messagebox.showinfo("Success", "Mail sent successfully.")
 
-# Create the main window
+# main window
 window = tk.Tk()
 window.title("Mail Application")
 
-# Define vibrant colors
-PRIMARY_COLOR = "#ff4500"  # Orange
-SECONDARY_COLOR = "#00aaff"  # Blue
-TEXT_COLOR = "#ffffff"  # White
+#color code
+PRIMARY_COLOR = "#ff4500"  
+SECONDARY_COLOR = "#00aaff"  
+TEXT_COLOR = "#ffffff"  
 
-# Configure window background color
 window.configure(bg=PRIMARY_COLOR)
 
-# Create labels and entry fields
 recipient_label = tk.Label(window, text="Recipient:", bg=PRIMARY_COLOR, fg=TEXT_COLOR)
 recipient_label.pack()
 recipient_entry = tk.Entry(window, width=90)
@@ -40,11 +35,10 @@ message_label.pack()
 message_text = scrolledtext.ScrolledText(window, height=20)
 message_text.pack()
 
-# Create the send button
 send_button = tk.Button(window, height=2, width=20, text="Send", bg=SECONDARY_COLOR, fg=TEXT_COLOR, command=send_mail)
 send_button.pack()
 
 
 
-# Run the main event loop
+#start point
 window.mainloop()
